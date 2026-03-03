0848f19940e4d40ef2658a98ca6f6d324cc483ec
Pankaj Kumar <pankaj.excelsior@gmail.com>
2026-03-03 16:14:21 +0530

    validator access modified

--- Sources/PaintNetwork/Network/Validator.swift
+++ Sources/PaintNetwork/Network/Validator.swift
@@ -6,14 +6,14 @@
 //
 import Foundation
 
-protocol Validator{
+public protocol Validator{
     associatedtype Input
     func validate(_ input: Input) throws -> Bool
 }
 
-struct EmailValidator: Validator {
-    typealias Input = String
-    func validate(_ input: String) -> Bool {
+public struct EmailValidator: Validator {
+    public typealias Input = String
+    public func validate(_ input: String) -> Bool {
         let emailRegex =
             #"^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"#
             let predicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
@@ -21,9 +21,9 @@ struct EmailValidator: Validator {
     }
 }
 
-struct PasswordValidator: Validator {
-    typealias Input = String
-    func validate(_ input: String) -> Bool {
+public struct PasswordValidator: Validator {
+    public typealias Input = String
+    public func validate(_ input: String) -> Bool {
         return input.count >= 5
     }
 }
